95c82941cbf8549b6b9d50fd2df0db9346957565
Shrimpey <kimo.edelbroek@gmail.com>
2025-09-12 11:39:36 +0200

    feat: inital api loop and logging

---
+++ Parking-api-new/api/server.py
@@ -0,0 +1,37 @@
+import asyncio
+import threading
+import uvicorn
+import constants
+from api.logger import Logger
+from api.apiroutes import Apiroutes
+
+class Main():
+
+    def __init__(self):
+        try:
+            self.logger = Logger().getLogger("Main")
+            self.logger.info("Initializing Main class")
+
+        except Exception as e:
+            self.logger.info(e)
+
+    def runApi(self):
+        self.logger.info("Starting API")
+        api = Apiroutes.run()  
+        uvicorn.run(api, host=constants.UVICORN_HOST_IP, port=constants.UVICORN_HOST_PORT)
+
+
+    async def main(self) -> None:
+
+        try:
+            apiThread = threading.Thread(target=self.runApi, daemon=True)
+            apiThread.start()
+
+        except Exception as e:
+            self.logger.info(e)
+
+
+if __name__ == "__main__":
+
+    main = Main()
+    asyncio.run(main.main())
