37f81706d153d6af5a534ebf534170bb4a8cb38d
Shrimpey304 <124566452+Shrimpey304@users.noreply.github.com>
2025-12-14 23:59:04 +0100

    write env file instead of sysvar

---
+++ Parking-api-new/api/entrypoint.sh
@@ -0,0 +1,14 @@
+set -e
+
+: "${FERNET_KEY:?FERNET_KEY is required}"
+: "${API_HOST_IP:?API_HOST_IP is required}"
+: "${API_HOST_PORT:?API_HOST_PORT is required}"
+
+cat > /app/api/.env <<EOF
+FERNET_KEY=${FERNET_KEY}
+API_HOST_IP=${API_HOST_IP}
+API_HOST_PORT=${API_HOST_PORT}
+EOF
+
+
+exec uvicorn server:app --host "${API_HOST_IP}" --port "${API_HOST_PORT}"
