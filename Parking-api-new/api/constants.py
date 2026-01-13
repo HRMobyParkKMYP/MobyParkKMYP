518264d15e80fa4e40dc4462731f21033448af65
Shrimpey <kimo.edelbroek@gmail.com>
2026-01-13 09:58:57 +0100

    fix?

--- Parking-api-new/api/constants.py
+++ Parking-api-new/api/constants.py
@@ -9,7 +9,7 @@ SYSTEMLOGS_DIR = os.path.join(MAIN_DIR, 'systemlogs')
 MAX_BACKUPS = 4 
 MAX_LOG_SIZE = 5 * 1024 * 1024  
 
-UVICORN_HOST_IP = environment["API_HOST_IP"] 
-UVICORN_HOST_PORT = int(environment["API_HOST_PORT"])
+UVICORN_HOST_IP = environment.get("API_HOST_IP") or os.getenv("API_HOST_IP", "0.0.0.0")
+UVICORN_HOST_PORT = int(environment.get("API_HOST_PORT") or os.getenv("API_HOST_PORT", "8000"))
 
-FERNET_KEY = environment["FERNET_KEY"] 
+FERNET_KEY = environment.get("FERNET_KEY") or os.getenv("FERNET_KEY", "") 
